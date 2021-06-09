cdb54af1a5cf88c3923283c89aa2c8ce6223c17b
Faisal Karim <Faisalmohammed2002@gmail.com>
2021-06-09 16:51:10 +1000

    The notification logic in the settings fragment is properly set up and once the preferences are reset, the fragment does refresh to indicate the change.

--- app/src/main/java/com/zeta/dailyachievementlist/ui/SettingsFragment.kt
+++ app/src/main/java/com/zeta/dailyachievementlist/ui/SettingsFragment.kt
@@ -2,6 +2,7 @@ package com.zeta.dailyachievementlist.ui
 
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
+import androidx.navigation.fragment.findNavController
 import androidx.preference.ListPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
@@ -11,8 +12,33 @@ import com.zeta.dailyachievementlist.ui.customPref.TimePreferenceDialogFragmentC
 
 class SettingsFragment : PreferenceFragmentCompat(), Preference.OnPreferenceChangeListener {
 
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+        val notificationTypePref = findPreference<ListPreference>(getString(R.string.pref_notification_type))
+        notificationTypePref?.onPreferenceChangeListener = this
+
+        setNotificationTypeDependentDisability(notificationTypePref?.value)
+
+        findPreference<Preference>(getString(R.string.pref_reset))?.setOnPreferenceClickListener {
+            preferenceManager.sharedPreferences.edit().clear().apply()
+            findNavController().navigate(R.id.action_navigation_settings_to_navigation_settings)
+            true
+        }
+    }
+
+    private fun setNotificationTypeDependentDisability(typeValue: Any?){
+        val notificationTypeOption = resources.getStringArray(R.array.notification_style_values)
+        val notificationTimePref = findPreference<TimePreference>(getString(R.string.pref_notification_time))
+        val notificationTimeStartPref = findPreference<TimePreference>(getString(R.string.pref_notification_start_time))
+        val notificationTimeEndPref = findPreference<TimePreference>(getString(R.string.pref_notification_end_time))
+        val notificationTimeIntervalPref = findPreference<ListPreference>(getString(R.string.pref_notification_interval))
+
+        notificationTimePref?.isEnabled =  typeValue == notificationTypeOption[1]
+        notificationTimeStartPref?.isEnabled = typeValue == notificationTypeOption[2]
+        notificationTimeEndPref?.isEnabled = typeValue == notificationTypeOption[2]
+        notificationTimeIntervalPref?.isEnabled = typeValue == notificationTypeOption[2]
     }
 
 
@@ -35,7 +61,11 @@ class SettingsFragment : PreferenceFragmentCompat(), Preference.OnPreferenceChan
     }
 
     override fun onPreferenceChange(preference: Preference?, newValue: Any?): Boolean {
-        TODO("Not yet implemented")
+        if(preference == null || newValue == null ) return true
+        if(preference.key.equals(getString(R.string.pref_notification_type))){
+           setNotificationTypeDependentDisability(newValue)
+        }
+        return true
     }
 
 
